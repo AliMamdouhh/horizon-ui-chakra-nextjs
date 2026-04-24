8f5055d77b9a4a34b1fdeffb134763b0d45504bf
AliMamdouhh <103014568+AliMamdouhh@users.noreply.github.com>
2026-04-24 23:34:54 +0300

    Change TypeScript error suppression from expect-error to ignore

--- src/components/charts/LineAreaChart.tsx
+++ src/components/charts/LineAreaChart.tsx
@@ -9,7 +9,7 @@ const LineAreaChart = (props: any) => {
   const { chartData, chartOptions } = props;
 
   return (
-    // @ts-expect-error
+    // @ts-ignore
     <Chart
       options={chartOptions}
       type="area"
